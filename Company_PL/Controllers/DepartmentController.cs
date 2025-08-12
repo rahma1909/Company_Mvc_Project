b03e895b3911bb7a054c4c754a992e2b9bd7926d
roma1909 <rahma.abulfatouh1@gmail.com>
2025-08-13 00:01:09 +0300

    implementation of Delete

--- Company_PL/Controllers/DepartmentController.cs
+++ Company_PL/Controllers/DepartmentController.cs
@@ -53,5 +53,138 @@ namespace Company_PL.Controllers
             }
             return View(model);
         }
+
+
+        [HttpGet]
+        public IActionResult Details(int? id,string ViewName="Details")
+        {
+            if (id == null)
+            {
+                return BadRequest("invalid id");
+            }
+   
+            
+            var dep=  _depRepo.Get(id.Value);
+
+            if(dep is null)
+             return NotFound($"dep with id {id} is not found");
+
+
+            return View(ViewName,dep);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+
+            //if (id == null)
+            //{
+            //    return BadRequest("invalid id");
+            //}
+
+
+            //var dep = _depRepo.Get(id.Value);
+
+            //if (dep is null)
+            //    return NotFound($"dep with id {id} is not found");
+
+
+            return Details(id,"Edit");
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit([FromRoute] int id, Department department)
+        {
+
+            if (ModelState.IsValid)
+            {
+                if (id == department.Id)
+                {
+                    var count = _depRepo.Update(department);
+
+
+                    if (count > 0) return RedirectToAction("Index");
+
+                }
+
+            }
+            return View(department);
+
+
+        }
+
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken] //with any post action
+        //public IActionResult Edit([FromRoute] int id, DepartmentEditDTO DTODepEdit)
+        //{
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        var department = new Department()
+        //        {
+        //            Id = id,
+        //            Code=DTODepEdit.Code,
+        //            Name=DTODepEdit.Name,
+        //            CreateAt=DTODepEdit.CreateAt
+        //        };
+
+        //            var count = _depRepo.Update(department);
+
+
+        //            if (count > 0) return RedirectToAction("Index");
+
+
+
+        //    }
+        //    return View(DTODepEdit);
+
+
+        //}
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+
+            //if (id == null)
+            //{
+            //    return BadRequest("invalid id");
+            //}
+
+
+            //var dep = _depRepo.Get(id.Value);
+
+            //if (dep is null)
+            //    return NotFound($"dep with id {id} is not found");
+
+
+            return Details(id,"Delete");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete([FromRoute] int id, Department department)
+        {
+
+            if (ModelState.IsValid)
+            {
+                if (id == department.Id)
+                {
+                    var count = _depRepo.Delete(department);
+
+
+                    if (count > 0) return RedirectToAction("Index");
+
+                }
+
+            }
+            return View(department);
+
+
+        }
+
+
     }
 }
---
+++ Company_PL/Dtos/DepartmentEditDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company_PL.Dtos
+{
+    public class DepartmentEditDTO
+    {
+        [Required(ErrorMessage = "code is required")]
+        public string Code { get; set; }
+        [Required(ErrorMessage = "name is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "date is required")]
+        public DateTime CreateAt { get; set; }
+    }
+}
