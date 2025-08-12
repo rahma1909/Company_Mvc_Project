35daeadf85ddc8a5cd2bfb755207709dcae5c20c
roma1909 <rahma.abulfatouh1@gmail.com>
2025-08-12 15:52:39 +0300

    create for create action and view

--- Company_PL/Controllers/DepartmentController.cs
+++ Company_PL/Controllers/DepartmentController.cs
@@ -1,21 +1,57 @@
 ﻿using Company_BLL.Interfaces;
 using Company_BLL.Repositories;
+using Company_DAL.Models;
+using Company_PL.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company_PL.Controllers
 {
     public class DepartmentController : Controller
     {
-       private readonly IDepartmentRepository _depRepo;
+        private readonly IDepartmentRepository _depRepo;
         public DepartmentController(IDepartmentRepository departmentRepository)
         {
             _depRepo = departmentRepository;
         }
+
+
         public IActionResult Index()
         {
-    
+
             var departments = _depRepo.GetAll();
             return View(departments);
+        }
+
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+
+
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDTO model)
+        {
+            if (ModelState.IsValid) //server side validation
+            {
+                var department = new Department()
+                {
+                    Code=model.Code,
+                    Name = model.Name,
+                    CreateAt = model.CreateAt
+                };
+                //manual mapping
+              var count=  _depRepo.Add(department);
+                if (count > 0)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(model);
         }
     }
 }
---
+++ Company_PL/Dtos/CreateDepartmentDTO.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Company_PL.Dtos
+{
+
+    //data from fe
+    public class CreateDepartmentDTO
+    {
+        [Required(ErrorMessage ="code is required")]
+        public string Code { get; set; }
+        [Required(ErrorMessage = "name is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "date is required")]
+        public DateTime CreateAt { get; set; }
+    }
+}
